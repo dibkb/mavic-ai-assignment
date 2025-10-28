abbae4c005197ed8afd1c1bedbf355965f08ccf8
dibkb <dibas9110@gmail.com>
2025-10-28 18:41:15 +0530

    feat: mod-wrap

---
+++ ai/workflows/mood-step.ts
@@ -0,0 +1,48 @@
+import { createStep } from "@mastra/core/workflows";
+import { z } from "zod";
+import creativityAgent from "../agents/creativity-agent";
+import openai from "../providers/open-ai";
+
+const moodWorkflow = createStep({
+  id: "mood-workflow",
+  description: "Evaluates the mood of a generated image",
+  inputSchema: z.object({
+    originalPrompt: z.string(),
+    imageUrl: z.url(),
+  }),
+  outputSchema: z.object({
+    score: z.number(),
+    moodPrompt: z.array(z.string()),
+    moodImage: z.array(z.string()),
+    matchScore: z.number(),
+  }),
+  execute: async ({ inputData }) => {
+    if (!inputData) {
+      throw new Error("Trigger data not found");
+    }
+    const { originalPrompt, imageUrl } = inputData;
+
+    const response = await creativityAgent.generate(
+      [
+        {
+          role: "user",
+          content: `Original prompt: ${originalPrompt}\nImage URL: ${imageUrl}`,
+        },
+      ],
+      {
+        structuredOutput: {
+          schema: z.object({
+            score: z.number(),
+            moodPrompt: z.array(z.string()),
+            moodImage: z.array(z.string()),
+            matchScore: z.number(),
+          }),
+          model: openai("gpt-4.1-nano"),
+        },
+      }
+    );
+    return response.object;
+  },
+});
+
+export default moodWorkflow;
