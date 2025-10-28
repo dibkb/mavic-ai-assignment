5793f6eaeb887b12944486c7b04083506b94c4b5
dibkb <dibas9110@gmail.com>
2025-10-28 18:36:20 +0530

    feat: size workflow.ts

---
+++ ai/workflows/sizeWorkflow.ts
@@ -0,0 +1,45 @@
+import { createStep } from "@mastra/core/workflows";
+import { z } from "zod";
+import sizeAgent from "../agents/size-agent";
+import openai from "../providers/open-ai";
+const sizeWorkflow = createStep({
+  id: "fetch-weather",
+  description: "Fetches weather forecast for a given city",
+  inputSchema: z.object({
+    originalPrompt: z.string(),
+    imageUrl: z.url(),
+  }),
+  outputSchema: z.object({
+    score: z.number(),
+    matches: z.boolean(),
+    details: z.string(),
+  }),
+  execute: async ({ inputData }) => {
+    if (!inputData) {
+      throw new Error("Trigger data not found");
+    }
+    const { originalPrompt, imageUrl } = inputData;
+
+    const response = await sizeAgent.generate(
+      [
+        {
+          role: "user",
+          content: `Original prompt: ${originalPrompt}\nImage URL: ${imageUrl}`,
+        },
+      ],
+      {
+        structuredOutput: {
+          schema: z.object({
+            score: z.number(),
+            matches: z.boolean(),
+            details: z.string(),
+          }),
+          model: openai("gpt-4.1-nano"),
+        },
+      }
+    );
+    return response.object;
+  },
+});
+
+export default sizeWorkflow;
